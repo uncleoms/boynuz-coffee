5152db3a24fd2ee373b2db9a047fa8c096bd0cee
uncleoms <omersaglam380@gmail.com>
2026-04-26 01:41:32 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        display: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
